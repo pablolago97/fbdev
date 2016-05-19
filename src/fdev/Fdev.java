12299d1bd93ecb2d63c38f64283a21e86bb0bb34
oracle <oracle@atanasoff08.danielcastelao.org>
2016-05-19 10:06:17 +0200

    Commit final

--- src/fdev/Fdev.java
+++ src/fdev/Fdev.java
@@ -3,23 +3,25 @@
 package fdev;
 
 import facebook4j.Facebook;
+import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
+import facebook4j.auth.AccessToken;
+import static java.awt.SystemColor.menu;
 
 
 public class Fdev  {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FacebookException {
         
-        //Instancia
-        Facebook facebook = new FacebookFactory().getInstance();
-        
-        facebook.getOAuthAppAccessToken();
-        
-        facebook.setOAuthAppId(appId, appSecret);
-        facebook.setOAuthPermissions(commaSeparetedPermissions);
-        facebook.setOAuthAccessToken(new AccessToken(accessToken, null));
-        
+ /**
+  * Instancia de la JFrame
+  */
+        Menu fdev = new Menu();
+        /**
+         * JFrame visible
+         */
+        fdev.setVisible(true);
     }
     
 }
